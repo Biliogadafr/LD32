556ec8c8e6dce0d01054f6213ab921aeb8033226
Biliogadafr <Biliogadafr@gmail.com>
2015-04-18 15:57:04 +0300

    Teleport for player. Mouse handling.

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,12 +6,23 @@ extends RigidBody2D
 # var b="textvar"
 
 export var speed = 0.0
+var lastPos = Vector2(0,0)
+
+export var teleportCooldown = 0.3;
+export var teleportLength = 100;
+var teleportCooldownCurrent = teleportCooldown;
 
 func _ready():
-	set_fixed_process(true);
+	print("hello 3")
+	set_process_input(true)
+	set_fixed_process(true)
 	pass
 	
 func _fixed_process(delta):
+	#aiming
+	var globalAimPos = get_node("Camera2D").get_canvas_transform().xform_inv(lastPos)
+	set_rot( get_pos().angle_to_point( globalAimPos ) )
+	#moving
 	var move_left = Input.is_action_pressed("move_left")
 	var move_right = Input.is_action_pressed("move_right")
 	var move_up = Input.is_action_pressed("move_up")
@@ -20,4 +31,22 @@ func _fixed_process(delta):
 	direction = direction.normalized()
 	direction*=delta*speed
 	set_linear_velocity(direction)
-	#set_pos(pos)
+	#teleporting
+	if Input.is_action_pressed("teleport") && teleportCooldownCurrent <= 0:
+		teleportCooldownCurrent = teleportCooldown
+		var teleportDirection = globalAimPos - get_pos()
+		print(teleportDirection)
+		teleportDirection = teleportDirection.normalized()
+		print(teleportDirection)
+		set_pos(get_pos() + teleportDirection * teleportLength)
+	else:
+		teleportCooldownCurrent -= delta
+	#hacking
+	if Input.is_action_pressed("hack"):
+		var bodies = get_node("HackHitBox").get_overlapping_bodies()
+		for body in bodies:
+			print(body);
+		
+func _input(ev):
+	if(ev.type == InputEvent.MOUSE_MOTION):
+		lastPos = ev.pos
